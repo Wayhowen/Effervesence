2f6861146b1649571e9fbd9164e3b382fc9e3f99
Jakub Sowa <jakub.sowa5@gmail.com>
2022-09-12 15:09:14 +0200

    files for osmanito

---
+++ LineFollower/sokoban_solver/input_reader.py
@@ -0,0 +1,31 @@
+from map_representation import Map
+
+class InputReader:
+    def __init__(self, file_path):
+        self._raw_input = self._read_input(file_path)
+        
+        self._maps = self._map_input_to_representation()
+
+    def _read_input(self, file_path):
+        with open(file_path, "r") as map_file:
+            lines = map_file.readlines()
+            return [line.rstrip() for line in lines]
+        
+    def _map_input_to_representation(self):
+        map_textual_representation = []
+        maps = []
+        for index, line in enumerate(self._raw_input):
+            if line.startswith(";") and index != 0:
+                maps.append(Map(map_textual_representation))
+                map_textual_representation = []
+            if len(line) > 0:
+                map_textual_representation.append(line)
+        return maps
+
+    def print_map(self):
+        print(self._maps[0])
+
+
+if __name__ == "__main__":
+    ir = InputReader("map_input.txt")
+    ir.print_map()
---
+++ LineFollower/sokoban_solver/map_representation.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Map:
+    def __init__(self, textual_representation):
+        self.name = ""
+        self.map = []
+        self._textual_representation_to_map(textual_representation)
+        
+    def _textual_representation_to_map(self, textual_representation: List[str]):
+        for line in textual_representation:
+            if line.startswith(";"):
+                self.name = line.strip("; ")
+            else:
+                self.map.append([e for e in line])
+        
+    
+    def __str__(self) -> str:
+        for row in self.map:
+            print(row)
