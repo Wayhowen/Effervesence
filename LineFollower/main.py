8939f00c3234b3f3212ea663e617284568e8491b
Jakub Sowa <jakub.sowa5@gmail.com>
2022-09-14 15:45:18 +0200

    new controls

--- LineFollower/main.py
+++ LineFollower/main.py
@@ -16,6 +16,7 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor, InfraredSensor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
+from pybricks.nxtdevices import LightSensor
 
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
@@ -29,29 +30,56 @@ left_sensor = ColorSensor(Port.S1)
 right_sensor = ColorSensor(Port.S4)
 
 # Initialize infrared sensors
-# ir_sensor = ColorSensor(Port.S2)
+light_sensor = LightSensor(Port.S2)
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=120)
 
-
-# def detectionCouroutine(robot: DriveBase, color_sensor: ColorSensor):
-#     if color_sensor.color() == Color.BLACK:
-#         robot.
+commands = ["straight", "left", "straight", "right", "straight",
+            "left", "straight", "right", "straight", "right", "right"]
 
 
-while True:
-    # turned = False
-    lv = left_sensor.color()
-    rv = right_sensor.color()
+class Controls:
+    def __init__(self, left_sensor, right_sensor, light_sensor):
+        self.left_sensor = left_sensor
+        self.right_sensor = right_sensor
+        self.light_sensor = light_sensor
 
-    # if lv == Color.BLACK and rv == Color.BLACK:
-    #    robot.drive(-75, 0)
-    #     robot.turn(90)
-    #     robot.straight(-150)
-    if lv == Color.BLACK:
-        robot.drive(50, 45)
-    elif rv == Color.BLACK:
-        robot.drive(50, -45)
-    else:
-        robot.drive(100, 0)
+    def perform_command(self, current_command: str):
+        if current_command == "straight":
+            self.drive_straight()
+        elif current_command == "left" or current_command == "right":
+            self.turn(current_command)
+
+    def drive_straight(self):
+        while True:
+            reflection = self.light_sensor.reflection()
+            if reflection > 8:
+                robot.drive(50, -5)
+            elif reflection < 6:
+                robot.drive(50, 5)
+
+            lv = self.left_sensor.color()
+            rv = self.right_sensor.color()
+            if (lv == Color.BLACK or rv == Color.BLACK):
+                ev3.speaker.beep()
+                robot.straight(100)
+                break
+
+    def turn(self, command: str):
+        if command == "right":
+            robot.turn(-90)
+            while 6 < self.light_sensor.reflection() > 8:
+                robot.turn(-2)
+        elif command == "left":
+            robot.turn(90)
+            while 6 < self.light_sensor.reflection() > 8:
+                robot.turn(2)
+            
+
+
+if __name__ == "__main__":
+    controls = Controls(left_sensor, right_sensor, light_sensor)
+    for command in commands:
+        controls.perform_command(command)
+
