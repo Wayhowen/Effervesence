48c965009206dcc205f0e12a2f5934bac7a3d221
Jakub Sowa <jakub.sowa5@gmail.com>
2022-09-19 20:08:39 +0200

    feat: added more code for the controller

--- LineFollower/main.py
+++ LineFollower/main.py
@@ -46,12 +46,22 @@ commands = [
 
 right_directions = ["N", "E", "S", "W"]
 left_directions = ["N", "W", "S", "E"]
-# commands = ["straight", "left", "straight", "right", "straight",
-#             "left", "straight", "right", "straight", "right", "right"]
 
 TURN_BY = 95
+DRIVE_SPEED = 70
+DRIVE_ADJUSTMENT_ANGLE = 5
+LOWER_REFLECTION_BOUNDARY = 6
+UPPER_REFLECTION_BOUNDARY = 8
 
-
+"""
+possible commands are:
+straight
+push
+left
+right
+turnaround
+backwards -- needs implementation
+"""
 class Controls:
     def __init__(self, left_sensor, right_sensor, light_sensor):
         self.left_sensor = left_sensor
@@ -71,6 +81,9 @@ class Controls:
         elif next_position[1] < self._current_position[1]:
             return "W"
 
+    """
+    This is basically a plan translator
+    """
     def _prepare_commands_to_reach_position(self,
                                             next_position,
                                             direction_to_go: str
@@ -78,20 +91,24 @@ class Controls:
         commands = []
         going_left = False
         if self._direction != direction_to_go:
+            # find how many turns we have to do
             current_position_index = right_directions.index(self._direction)
             next_position_index = right_directions.index(direction_to_go)
             position_difference = abs(current_position_index - next_position_index)
+            # if more than 2, then there is a better alternative available surely
             if position_difference > 2:
                 current_position_index = left_directions.index(self._direction)
                 next_position_index = left_directions.index(direction_to_go)
                 position_difference = abs(current_position_index - next_position_index)
                 going_left = True
                 
-            for _ in range(position_difference):
+            if position_difference == 1:
                 if going_left or current_position_index > next_position_index:
                     commands.append("left")
                 else:
                     commands.append("right")
+            elif position_difference == 2:
+                commands.append("turnaround")
 
         if next_position[2] == 1:
             commands.append("push")
@@ -112,32 +129,57 @@ class Controls:
             if command == "straight":
                 self.drive_straight()
                 self._current_position = current_command
-            elif command == "left" or command == "right":
+            elif command == "push":
+                self.drive_straight(True)
+                self._current_position = current_command
+            elif command == "left" or command == "right" or command == "turnaround":
                 self.turn(command)
 
-    def drive_straight(self):
+    def drive_straight(self, pushing = False):
         while True:
             reflection = self.light_sensor.reflection()
-            if reflection > 8:
-                robot.drive(70, -5)
-            elif reflection < 6:
-                robot.drive(70, 5)
+            if reflection > UPPER_REFLECTION_BOUNDARY:
+                robot.drive(DRIVE_SPEED, -DRIVE_ADJUSTMENT_ANGLE)
+            elif reflection < LOWER_REFLECTION_BOUNDARY:
+                robot.drive(DRIVE_SPEED, DRIVE_ADJUSTMENT_ANGLE)
 
             lv = self.left_sensor.reflection()
             rv = self.right_sensor.reflection()
-            if (lv < 8 or rv < 8):
+            if (lv < UPPER_REFLECTION_BOUNDARY or rv < UPPER_REFLECTION_BOUNDARY):
                 ev3.speaker.beep()
-                robot.straight(165)
-                break
+                if not pushing:
+                    robot.straight(165)
+                    break
+                self.drive_backwards()
+            # just a debugging statement
             else:
                 ev3.screen.clear()
                 ev3.screen.print(lv)
                 ev3.screen.print(rv)
+    
+    # TODO: inverse of forwards, not tested
+    def drive_backwards(self):
+        while True:
+            reflection = self.light_sensor.reflection()
+            if reflection > UPPER_REFLECTION_BOUNDARY:
+                robot.drive(DRIVE_SPEED, DRIVE_ADJUSTMENT_ANGLE)
+            elif reflection < LOWER_REFLECTION_BOUNDARY:
+                robot.drive(DRIVE_SPEED, -DRIVE_ADJUSTMENT_ANGLE)
 
+            lv = self.left_sensor.reflection()
+            rv = self.right_sensor.reflection()
+            if (lv < UPPER_REFLECTION_BOUNDARY or rv < UPPER_REFLECTION_BOUNDARY):
+                ev3.speaker.beep()
+                robot.straight(165)
+                break
+
+    """
+    This if statements allow for easier choosing of the current directions after turn
+    """
     def turn(self, command: str):
         if command == "right":
             robot.turn(-TURN_BY)
-            while 6 < self.light_sensor.reflection() > 8:
+            while LOWER_REFLECTION_BOUNDARY < self.light_sensor.reflection() > UPPER_REFLECTION_BOUNDARY:
                 robot.turn(-2)
             
             if right_directions.index(self._direction) + 1 != len(right_directions):
@@ -145,14 +187,28 @@ class Controls:
             else:
                 self._direction = left_directions[left_directions.index(self._direction) - 1]
         elif command == "left":
+            # TODO: this is where the code fails with plows
             robot.turn(TURN_BY + 10)
-            while 6 < self.light_sensor.reflection() > 8:
+            while LOWER_REFLECTION_BOUNDARY < self.light_sensor.reflection() > UPPER_REFLECTION_BOUNDARY:
                 robot.turn(2)
                 
             if right_directions.index(self._direction) != 0:
                 self._direction = right_directions[right_directions.index(self._direction) - 1]
             else:
                 self._direction = left_directions[left_directions.index(self._direction) + 1]
+                
+        elif command == "turnaround":
+            robot.turn(2 * TURN_BY)
+            while LOWER_REFLECTION_BOUNDARY < self.light_sensor.reflection() > UPPER_REFLECTION_BOUNDARY:
+                robot.turn(2)
+            if self._direction == "N":
+                self._direction = "S"
+            elif self._direction == "E":
+                self._direction = "W"
+            elif self._direction == "S":
+                self._direction = "N"
+            elif self._direction == "W":
+                self._direction = "E"
 
 if __name__ == "__main__":
     controls = Controls(left_sensor, right_sensor, light_sensor)
