64c668ffea41bc44b9cf38e02fd95a40dbcf861d
Jakub Sowa <jakub.sowa5@gmail.com>
2022-09-21 14:15:34 +0200

    robot mapping inb4

--- LineFollower/copy2.py
+++ LineFollower/copy2.py
@@ -37,7 +37,8 @@ light_sensor = LightSensor(Port.S2)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=80, axle_track=170)
 
 # Claire
-commands =  [(0, 0, 0), (1, 0, 0), (2, 0, 0), (3, 0, 0), (3, 1, 1), (3, 2, 1), (2, 2, 0), (2, 1, 1), (1, 1, 1), (1, 0, 0), (0, 0, 0), (0, 1, 1), (0, 2, 1)]
+commands = [(0, 0, 0), (1, 0, 0), (2, 0, 0), (3, 0, 0), (3, 1, 1), (3, 2, 1),
+            (2, 2, 0), (2, 1, 1), (1, 1, 1), (1, 0, 0), (0, 0, 0), (0, 1, 1), (0, 2, 1)]
 
 # commands = [
 #     (1, 0, 0),
@@ -51,7 +52,7 @@ right_directions = ["N", "E", "S", "W"]
 left_directions = ["N", "W", "S", "E"]
 
 TURN_BY = 95
-DRIVE_SPEED = 70
+DRIVE_SPEED = 140
 DRIVE_ADJUSTMENT_ANGLE = 5
 LOWER_REFLECTION_BOUNDARY = 6
 UPPER_REFLECTION_BOUNDARY = 8
@@ -66,6 +67,8 @@ right
 turnaround
 backwards -- needs implementation
 """
+
+
 class Controls:
     def __init__(self, current_position, left_sensor, right_sensor, light_sensor):
         self.left_sensor = left_sensor
@@ -74,8 +77,6 @@ class Controls:
 
         self._current_position = current_position
         self._direction = "S"
-        
-        self._directions = []
 
     def _choose_direction_to_go(self, next_position) -> str:
         if next_position[0] < self._current_position[0]:
@@ -90,6 +91,7 @@ class Controls:
     """
     This is basically a plan translator
     """
+
     def _prepare_commands_to_reach_position(self,
                                             next_position,
                                             direction_to_go: str
@@ -98,50 +100,41 @@ class Controls:
         if self._direction != direction_to_go:
             current_position_index = right_directions.index(self._direction)
             if (direction_to_go == right_directions[(current_position_index + 1) % 4]):
-                commands.append("right")    
-                self._direction = right_directions[(right_directions.index(self._direction) + 1) % 4]
+                commands.append("right")
             elif (direction_to_go == right_directions[(current_position_index - 1) % 4]):
                 commands.append("left")
-                self._direction = right_directions[(right_directions.index(self._direction) - 1) % 4]
             else:
                 commands.append("turnaround")
-                if self._direction == "N":
-                    self._direction = "S"
-                elif self._direction == "E":
-                    self._direction = "W"
-                elif self._direction == "S":
-                    self._direction = "N"
-                elif self._direction == "W":
-                    self._direction = "E"
 
         if next_position[2] == 1:
             commands.append("push")
-            self._current_position = next_position
         else:
             commands.append("straight")
-            self._current_position = next_position
-            
 
         return commands
 
-    def perform_command(self, command):
-        if command == "straight":
-            self.drive_straight()
-        elif command == "push":
-            self.drive_straight()
-            self.drive_straight(True)
-        elif command == "double_push":
-            self.drive_straight()
-            self.drive_straight()
-            self.drive_straight(True)
-        elif command == "left" or command == "right" or command == "turnaround":
-            self.turn(command)
-            # ev3.screen.clear()
-            # ev3.screen.print(self._direction)
-            # ev3.screen.print(self._current_position)
-            # ev3.screen.print(direction_to_go)
+    def perform_command(self, current_command):
+        direction_to_go = self._choose_direction_to_go(current_command)
 
-    def drive_straight(self, pushing = False):
+        commands_to_perform = self._prepare_commands_to_reach_position(
+            current_command, direction_to_go)
+
+        for command in commands_to_perform:
+            if command == "straight":
+                self.drive_straight()
+                self._current_position = current_command
+            elif command == "push":
+                self.drive_straight()
+                self.drive_straight(True)
+                self._current_position = current_command
+            elif command == "left" or command == "right" or command == "turnaround":
+                self.turn(command)
+            ev3.screen.clear()
+            ev3.screen.print(self._direction)
+            ev3.screen.print(self._current_position)
+            ev3.screen.print(direction_to_go)
+
+    def drive_straight(self, pushing=False):
         while True:
             reflection = self.light_sensor.reflection()
             if reflection > UPPER_REFLECTION_BOUNDARY:
@@ -156,18 +149,18 @@ class Controls:
                 if not pushing:
                     robot.straight(140)
                     break
-                
+
                 self.drive_backwards()
                 break
             # just a debugging statement
-    
+
     # TODO: inverse of forwards, not tested
     def drive_backwards(self):
         robot.straight(-80)
         self.turn("turnaround")
         robot.straight(-80)
         self.drive_straight()
-        
+
     def good_drive_backwards(self):
         stamp = time.time()
         while True:
@@ -176,30 +169,44 @@ class Controls:
                 robot.drive(-DRIVE_SPEED, -DRIVE_ADJUSTMENT_ANGLE)
             elif reflection < LOWER_REFLECTION_BOUNDARY:
                 robot.drive(-DRIVE_SPEED, DRIVE_ADJUSTMENT_ANGLE)
-            
+
             if time.time() > stamp + DRIVE_BACKWARDS_FOR_SECONDS:
                 break
-
 
     """
     This if statements allow for easier choosing of the current directions after turn
     """
+
     def turn(self, command: str):
         if command == "right":
             robot.turn(-TURN_BY)
             while LOWER_REFLECTION_BOUNDARY < self.light_sensor.reflection() > UPPER_REFLECTION_BOUNDARY:
                 robot.turn(-2)
-            
+
+            self._direction = right_directions[(
+                right_directions.index(self._direction) + 1) % 4]
         elif command == "left":
             # TODO: this is where the code fails with plows
             robot.turn(TURN_BY)
             while LOWER_REFLECTION_BOUNDARY < self.light_sensor.reflection() > UPPER_REFLECTION_BOUNDARY:
                 robot.turn(2)
-                            
+
+            self._direction = right_directions[(
+                right_directions.index(self._direction) - 1) % 4]
+
         elif command == "turnaround":
             robot.turn(-1.75 * TURN_BY)
             while LOWER_REFLECTION_BOUNDARY < self.light_sensor.reflection() > UPPER_REFLECTION_BOUNDARY:
                 robot.turn(-2)
+            if self._direction == "N":
+                self._direction = "S"
+            elif self._direction == "E":
+                self._direction = "W"
+            elif self._direction == "S":
+                self._direction = "N"
+            elif self._direction == "W":
+                self._direction = "E"
+
 
 if __name__ == "__main__":
     controls = Controls(commands[0], left_sensor, right_sensor, light_sensor)
@@ -208,30 +215,5 @@ if __name__ == "__main__":
     #     ev3.screen.print(left_sensor.reflection())[
     #     time.sleep(0.5)
     #     ev3.screen.clear()
-    
-    
-    robot_commands = []
     for command in commands[1:]:
-        direction_to_go = controls._choose_direction_to_go(command)
-
-        commands_to_perform = controls._prepare_commands_to_reach_position(
-            command, direction_to_go)
-        robot_commands.extend(commands_to_perform)
-    
-    print(robot_commands)
-    shortened_commands = []
-    skip_next = False
-    for i, rc in enumerate(robot_commands):
-        if i <= len(robot_commands):
-            if skip_next:
-                skip_next = False
-            elif robot_commands[i + 1] == rc and rc == "push":
-                shortened_commands.append("double_push")
-                skip_next = True
-            else:
-                shortened_commands.append(rc)
-    
-    print(shortened_commands)
-        
-    for robot_command in shortened_commands:
-        controls.perform_command(robot_command)
+        controls.perform_command(command)
