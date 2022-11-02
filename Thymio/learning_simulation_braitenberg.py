6cde36ceca0f8d7551f66a989c2c1872a14f1d3b
Osman Abdinasir Hassan <osha@itu.dk>
2022-11-02 12:32:20 +0100

    better braitenberg state weights

--- Thymio/learning_simulation_braitenberg.py
+++ Thymio/learning_simulation_braitenberg.py
@@ -14,7 +14,7 @@ from simulator.simulator import Simulator
 
 
 if __name__ == '__main__':
-    states = ("INFRONT", "LEFT", "RIGHT", "EXPLORE")
+    states = ("INFRONT", "LEFT", "RIGHT", "EXPLORE", "BACKINGUP", "TURNING")
     actions = ("GOFORWARDS", "GOBACKWARDS", "GOLEFT", "GORIGHT")
     q_leaner = QLearner(states, actions, states.index("EXPLORE"))
     simulator = Simulator()
@@ -34,21 +34,30 @@ if __name__ == '__main__':
         simulator.step(controller)
 
         prox_horizontal = controller.distances_to_wall(simulator.world)
+        prox_odom_l, prox_odom_r = controller.get_speed()
         print(prox_horizontal[2])
         if prox_horizontal[2] < 0.49:
-            return states.index("INFRONT"), -1
+            return states.index("INFRONT"), 5
         elif prox_horizontal[0] < 0.49 or prox_horizontal[1] < 0.49:
-            return states.index("LEFT"), -1
+            return states.index("LEFT"), 10
         elif prox_horizontal[3] < 0.49 or prox_horizontal[4] < 0.49:
-            return states.index("RIGHT"), -1
+            return states.index("RIGHT"), 10
+        elif prox_odom_l+prox_odom_r < 0.0 :
+            return states.index("BACKINGUP"), 1
+        elif (prox_odom_l < 0.0 and prox_odom_r > 0.0) or (prox_odom_l > 0.0 and prox_odom_r < 0.0):
+            return states.index("TURNING"), 2
         else:
-            return states.index("EXPLORE"), 1
+            return states.index("EXPLORE"), 20
 
-    for cnt in range(10000):
-        q_leaner.learn(step_function)
-        # check collision with arena walls
-        if simulator.world.distance(Point(controller.x, controller.y)) < simulator.L / 2:
-            break
+    with open("animator/trajectory.dat", "w") as file:
+        for cnt in range(10000):
+            q_leaner.learn(step_function)
+            # check collision with arena walls
+            if simulator.world.distance(Point(controller.x, controller.y)) < simulator.L / 2:
+                break
+            
+            if cnt % 50 == 0:
+                file.write(f"{controller.x}, {controller.y}, {cos(controller.q) * 0.2}, {sin(controller.q) * 0.2}\n")
 
     print(q_leaner.q_table)
             #
