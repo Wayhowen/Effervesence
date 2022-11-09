a51d02da9c21202297b034c9916cdcec4535725d
Jakub Sowa <jakub.sowa5@gmail.com>
2022-11-09 23:37:25 +0100

    feat: made the speeds right

--- Thymio/simulator_main.py
+++ Thymio/simulator_main.py
@@ -10,36 +10,72 @@ from simulator.robot_model.controller import Controller
 from simulator import Simulator
 
 
+class Behaviors:
+    def __init__(self):
+        self.simulator = Simulator()
+        self.controller = Controller(self.simulator.W, self.simulator.H)
+
+        # used for speed measurment
+        self.distances = []
+
+    def step(self):
+        self.simulator.step(self.controller)
+
+    def wall_avoidance(self, step):
+        distance = self.controller.distances_to_wall(self.simulator.world)[2]
+        # simple controller - change direction of wheels every 10 seconds (100*robot_timestep) unless close to wall then turn on spot
+        if distance < 0.1:
+            self.controller.left_wheel_velocity = 1.445
+            self.controller.right_wheel_velocity = -1.445
+        else:
+            if step % 100 == 0:
+                self.controller.left_wheel_velocity = 1.445
+                self.controller.right_wheel_velocity = 1.445
+
+    def speed_measurment(self, step):
+        self.distances.append(self.controller.distances_to_wall(self.simulator.world)[2])
+
+        self.controller.left_wheel_velocity = 1.445
+        self.controller.right_wheel_velocity = 1.445
+
+        # print distance travelled in meters every 4 secs
+        if step % 40 == 0:
+            print(abs(self.distances[-1] - self.distances[0]))
+
+    def rotation_measurment(self, step):
+        self.distances.append(self.controller.q)
+
+        self.controller.left_wheel_velocity = 5.5241
+        self.controller.right_wheel_velocity = -5.5241
+
+        # print rotation in radians every 4 secs
+        if step % 48 == 0:
+            print(abs(self.distances[-1] - self.distances[0]))
+
+    @property
+    def position(self):
+        return f"{self.controller.x}, {self.controller.y}, {cos(self.controller.q) * 0.2}, {sin(self.controller.q) * 0.2}\n"
+
+
 if __name__ == '__main__':
-    simulator = Simulator()
-    controller = Controller(simulator.W, simulator.H)
+    behaviors = Behaviors()
     with open("animator/trajectory.dat", "w") as file:
         for cnt in range(5000):
             # simple single-ray sensor
             try:
-                on_the_line = controller.on_the_line(simulator.world)
-                # simple controller - change direction of wheels every 10 seconds (100*robot_timestep) unless close to wall then turn on spot
-                if on_the_line:
-                    controller.left_wheel_velocity = -0.4
-                    controller.right_wheel_velocity = -0.4
-                else:
-                    distance = controller.distances_to_wall(simulator.world)[2]
-
-                    if cnt % 100 == 0:
-                        controller.left_wheel_velocity = random()
-                        controller.right_wheel_velocity = random()
+                behaviors.rotation_measurment(cnt)
 
                 # step simulation
-                simulator.step(controller)
+                behaviors.step()
 
-                # check collision with arena walls
-                if on_the_line:
-                    file.write(f"{controller.x}, {controller.y}, {cos(controller.q) * 0.2}, {sin(controller.q) * 0.2}\n")
-                    break
+                # # check collision with arena walls
+                # if controller.on_the_line(simulator.world):
+                #     file.write(f"{controller.x}, {controller.y}, {cos(controller.q) * 0.2}, {sin(controller.q) * 0.2}\n")
+                #     break
 
                 if cnt % 50 == 0:
-                    file.write(f"{controller.x}, {controller.y}, {cos(controller.q) * 0.2}, {sin(controller.q) * 0.2}\n")
+                    file.write(behaviors.position)
             except AttributeError:
-                file.write(f"{controller.x}, {controller.y}, {cos(controller.q) * 0.2}, {sin(controller.q) * 0.2}\n")
+                file.write(behaviors.position)
                 print("out of bounds")
                 break
