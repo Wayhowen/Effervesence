c6755b70019dc9319d9bfe477391337386f44e11
Jakub Sowa <jakub.sowa5@gmail.com>
2022-11-30 18:18:17 +0100

    feat: some threading testing

--- Thymio/test_writing.py
+++ Thymio/test_writing.py
@@ -1,4 +1,26 @@
+import threading
+import time
+
 import numpy as np
+
+class One:
+    def __init__(self):
+        self.die = False
+
+    def run(self):
+        while not self.die:
+            time.sleep(0.5)
+        print("died")
+
+
+class Two:
+    def __init__(self):
+        pass
+
+    def run(self, c1):
+        time.sleep(5)
+        c1.die = True
+        print("set die")
 
 if __name__ == "__main__":
     # a = np.array([[1, 2], [3, 4]])
@@ -6,8 +28,16 @@ if __name__ == "__main__":
     # with open("test.txt", "ab") as file:
     #     np.save(file, a, False)
 
-    with open('test.txt', 'rb') as f:
-        a = np.load(f, allow_pickle=True)
-        b = np.load(f, allow_pickle=True)
-        print(a)
-        print(b)
+    # with open('test.txt', 'rb') as f:
+    #     a = np.load(f, allow_pickle=True)
+    #     b = np.load(f, allow_pickle=True)
+    #     print(a)
+    #     print(b)
+
+    one = One()
+    two = Two()
+
+    t1 = threading.Thread(target=two.run, args=(one, ))
+    t1.daemon = True
+    t1.start()
+    one.run()
